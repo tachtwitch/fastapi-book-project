c2dea20db25084bbb5e994b0c0e941fd4b64b21b
tahtwitch <gravitox22@gmailcom>
2025-02-10 17:17:40 +0100

    initial commit

--- api/routes/books.py
+++ api/routes/books.py
@@ -47,6 +47,15 @@ async def create_book(book: Book):
 async def get_books() -> OrderedDict[int, Book]:
     return db.get_books()
 
+@router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
+async def get_book(book_id:int):
+    book = db.get_book(book_id=book_id)
+    if(book == None):
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content= {"detail":"Book Not Found"}
+        )
+    return db.get_book(book_id=book_id)
 
 @router.put("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
 async def update_book(book_id: int, book: Book) -> Book:
